c0447ee156c572e52310e3780da4e8cd4312de59
seerapudeepthi <seerapudeepthi00@gmail.com>
2026-05-13 15:43:09 +0530

    Add files via upload
    day 1 basics :
    introduction of python
    tokens
    identifiers
    variables

---
+++ day 1.py
@@ -0,0 +1,53 @@
+#print("hello world")
+
+'''what is python:
+-------------------
+i)high level language:
+
+    no need to worry about low level management
+
+ii)dynamically typed language:
+    
+   no need to mention data type for variables
+
+   eg:num=23
+      so="python"
+      any=[1,2]
+      print(num)
+      print(so)
+      print(any)
+
+iii)interpreter language:
+
+     runs the code line by line
+
+tokens:
+------------
+   keywords in the programs
+   eg:if,else,while,for
+
+identifiers:
+-------------
+     name of variables,function and classes
+
+variables:
+--------------
+good ways:
+    A-Z,a-z,_,0-9
+    
+bad ways:
+    don't use at all( special characters,space),use anywhere but not in first position(numbers)
+'''
+name9 = "deepthi"
+roll_no = 23
+List = [1,2]
+print(name9)
+print(roll_no)
+print(List)
+
+
+
+
+
+
+
